af49521fb32e978e1ef4fb4de6a2947ef31ffc9f
Doug South <doug.south@gmail.com>
2012-06-15 14:41:38 +1000

    Looks like a keyboard, if not a bit too small

--- src/keyboard/gui.clj
+++ src/keyboard/gui.clj
@@ -2,8 +2,9 @@
   (:import [java.awt Color Dimension]
            [javax.swing JComponent]))
 
-(def key-width 25)
-(def key-height 125)
+(def key-width 24)
+(def key-height 110)
+(def blacks [0 1 3 4 5])
 (def component-width (inc (* 8 key-width)))
 (def component-height (inc key-height))
 
@@ -12,7 +13,14 @@
   (.fillRect g 0 0 component-width component-height)
   (.setColor g Color/BLACK)
   (doseq [x (range 8)]
-    (.drawRect g (* x key-width) 0 key-width key-height)))
+    (.drawRect g (* x key-width) 0 key-width key-height))
+  (doseq [b blacks]
+    (let [b-key-width (/ key-width 2)
+          x (+ (* 3 (/ b-key-width 2)) (* b key-width))
+          y 0
+          width b-key-width
+          height (* 2 (/ key-height 3))]
+      (.fillRect g x y width height))))
 
 (def keyboard-component
   (proxy [JComponent] []
