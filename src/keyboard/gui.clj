755a9b16ee6b6279a8d99ba196af639d5f355bac
Doug South <doug.south@gmail.com>
2012-06-15 14:16:34 +1000

    boxes that aren't keys (and not even white)

--- src/keyboard/gui.clj
+++ src/keyboard/gui.clj
@@ -1,18 +1,24 @@
 (ns keyboard.gui
-  (:import [java.awt Dimension]
+  (:import [java.awt Color Dimension]
            [javax.swing JComponent]))
 
 (def key-width 25)
-(def key-height 200)
+(def key-height 125)
 
-(defn keyboard-paint [g])
+(defn keyboard-paint [g]
+  (println "DRAWING :P")
+  (.setColor g Color/BLACK)
+  (doseq [x (range 8)]
+    (.drawRect g (* x key-width) 0 key-width key-height)))
 
 (def keyboard-component
   (proxy [JComponent] []
     (paintComponent [g]
+      (println "paint component")
       (proxy-super paintComponent g)
       (keyboard-paint g))))
 
 (defn get-keyboard-component []
-  (.setPreferredSize keyboard-component (Dimension. (* 8 key-width) key-height))
+  (.setPreferredSize keyboard-component (Dimension. (inc (* 8 key-width)) (inc key-height)))
+  (.setBackground keyboard-component Color/WHITE)
   keyboard-component)
